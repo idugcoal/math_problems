d49ab46d98fe38de07233bdb54cdcc96296029b0
Doug Cole <idugcoal@gmail.com>
2018-09-08 09:47:54 -0700

    cleanup

--- main.py
+++ main.py
@@ -53,14 +53,9 @@ questions = {
   29 : rtt.get_gb(),
   30 : rtt.get_tb(),
 }
-lenny = len(answers) - 1
+
 template = latex_jinja_env.get_template(in_file)
-renderedTemplate = template.render(
-  # q1=q1, q2=q2, q3=q3, q4=q4, q5=q5, q6=q6, q7=q7, q8=q8, q9=q9,
-  # q10=q10, q11=q11, q12=q12, q13=q13, q14=q14, q15=q15, q16=q16, 
-  # q17=q17, q18=q18, q19=q19, q20=q20, q21=q21, q22=q22, q23=q23, 
-  # q24=q24, q25=q25, q26=q26, q27=q27, q28=q28, q29=q29, q30=q30,
-  answers=answers, lenny=lenny, questions=questions)
+renderedTemplate = template.render(questions=questions, answers=answers)
 
 with open(out_file + '.tex', 'w') as f:
     f.write(renderedTemplate)
