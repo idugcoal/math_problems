112c77b223b053c015d6d04c75377fe54ac415e0
Doug Cole <idugcoal@gmail.com>
2018-09-08 09:43:21 -0700

    adds for loops to iterate through questions and answers

--- main.py
+++ main.py
@@ -19,42 +19,48 @@ latex_jinja_env = jinja2.Environment(
 
 in_file = 'math-template.tex'
 out_file = 'jinjatest'
-# q1 = rtt.get_rdb()
-# q1 = rtt.get_gb()
-q1 = rtt.get_tb()
-# q1 = rtt.get_tABx()
-q2 = rtt.get_tABo1()
-q3 = rtt.get_tABo2()
-q4 = rtt.get_tBCx()
-q5 = rtt.get_tBCo1()
-q6 = rtt.get_tBCo2()
-q7 = rtt.get_tACx()
-q8 = rtt.get_tACo1()
-q9 = rtt.get_tACo2()
-q10 = rtt.get_tAo1x()
-q11 = rtt.get_tAo1B()
-q12 = rtt.get_tAo1o2()
-q13 = rtt.get_tAo2x()
-q14 = rtt.get_tAo2B()
-q15 = rtt.get_tAo2o1()
-q16 = rtt.get_tBo1x()
-q17 = rtt.get_tBo1A()
-q18 = rtt.get_tBo1o2()
-q19 = rtt.get_tBo2x()
-q20 = rtt.get_tBo2A()
-q21 = rtt.get_tBo2o1()
-q22 = rtt.get_tCo1A()
-q23 = rtt.get_tCo1B()
-q24 = rtt.get_tCo1o2()
-q25 = rtt.get_tCo2A()
-q26 = rtt.get_tCo2B()
-q27 = rtt.get_tCo2o1()
+
+answers = {1: "cat", 2: "dog", 3: "kitty", 4: "puppy"}
+questions = {
+  1 : rtt.get_tABx(),
+  2 : rtt.get_tABo1(),
+  3 : rtt.get_tABo2(),
+  4 : rtt.get_tBCx(),
+  5 : rtt.get_tBCo1(),
+  6 : rtt.get_tBCo2(),
+  7 : rtt.get_tACx(),
+  8 : rtt.get_tACo1(),
+  9 : rtt.get_tACo2(),
+  10 : rtt.get_tAo1x(),
+  11 : rtt.get_tAo1B(),
+  12 : rtt.get_tAo1o2(),
+  13 : rtt.get_tAo2x(),
+  14 : rtt.get_tAo2B(),
+  15 : rtt.get_tAo2o1(),
+  16 : rtt.get_tBo1x(),
+  17 : rtt.get_tBo1A(),
+  18 : rtt.get_tBo1o2(),
+  19 : rtt.get_tBo2x(),
+  20 : rtt.get_tBo2A(),
+  21 : rtt.get_tBo2o1(),
+  22 : rtt.get_tCo1A(),
+  23 : rtt.get_tCo1B(),
+  24 : rtt.get_tCo1o2(),
+  25 : rtt.get_tCo2A(),
+  26 : rtt.get_tCo2B(),
+  27 : rtt.get_tCo2o1(),
+  28 : rtt.get_rdb(),
+  29 : rtt.get_gb(),
+  30 : rtt.get_tb(),
+}
+lenny = len(answers) - 1
 template = latex_jinja_env.get_template(in_file)
 renderedTemplate = template.render(
-  q1=q1, q2=q2, q3=q3, q4=q4, q5=q5, q6=q6, q7=q7, q8=q8, q9=q9,
-  q10=q10, q11=q11, q12=q12, q13=q13, q14=q14, q15=q15, q16=q16, 
-  q17=q17, q18=q18, q19=q19, q20=q20, q21=q21, q22=q22, q23=q23, 
-  q24=q24, q25=q25, q26=q26, q27=q27)
+  # q1=q1, q2=q2, q3=q3, q4=q4, q5=q5, q6=q6, q7=q7, q8=q8, q9=q9,
+  # q10=q10, q11=q11, q12=q12, q13=q13, q14=q14, q15=q15, q16=q16, 
+  # q17=q17, q18=q18, q19=q19, q20=q20, q21=q21, q22=q22, q23=q23, 
+  # q24=q24, q25=q25, q26=q26, q27=q27, q28=q28, q29=q29, q30=q30,
+  answers=answers, lenny=lenny, questions=questions)
 
 with open(out_file + '.tex', 'w') as f:
     f.write(renderedTemplate)
